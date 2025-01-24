f11e8b11a2e1eae8faeb18a85f5454f7caab6ae9
Sathish Dusharla <23eg105a16@anurag.edu.in>
2025-01-24 22:06:25 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
         
         <div className="mt-12 pt-8 border-t border-gray-800">
           <p className="text-center text-gray-400">
-            © {new Date().getFullYear()} BlockvoteX. All rights reserved.
+            © {new Date().getFullYear()} BlockVoteX. All rights reserved.
           </p>
         </div>
       </div>
